b16d5ec7ec8147e159fad8d5ca80a59796da1896
Jonathan Kolyer <jonathankolyer@gmail.com>
2020-01-24 18:09:37 -0800

    removed unused plugin functional

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,7 @@ module.exports = {
     },
     "plugins": [
       "@typescript-eslint",
-      "@typescript-eslint/tslint",
-      "functional"
+      "@typescript-eslint/tslint"
     ],
   "rules": {
         "@typescript-eslint/adjacent-overload-signatures": "error",

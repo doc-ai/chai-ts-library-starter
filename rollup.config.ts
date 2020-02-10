b79911e3b7269f50e0e4659afde78a9576e7e241
Jonathan Kolyer <jonathankolyer@gmail.com>
2020-02-09 22:04:39 -0800

    CHAI-19:  updated package versions and deprecated packages

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,9 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
@@ -33,7 +33,9 @@ export default {
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
+    resolve({
+      preferBuiltins: true,
+    }),
 
     // Resolve source maps to the original source
     sourceMaps(),
